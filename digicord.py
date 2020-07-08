9eebd767a8e918fbc15f31fe1ad3e01a38e1f06d
arn <ruenan@mail.uc.edu>
2020-07-08 16:25:50 -0400

    Created command to set spawn channel

--- digicord.py
+++ digicord.py
@@ -7,6 +7,9 @@ from redbot.core.data_manager import cog_data_path
 from redbot.core.bot import Red
 
 log = logging.getLogger("red.digicord")
+_DEFAULT_GUILD = {
+    "spawn_channel": None
+}
 
 
 class Digicord(commands.Cog):
@@ -15,6 +18,8 @@ class Digicord(commands.Cog):
     def __init__(self, bot:Red):
             super().__init__()
             self.bot = bot
+            self._conf = Config.get_conf(None, 90210, cog_name=f"{self.__class__.__name__}", force_registration=True)
+            self._conf.register_guild(**_DEFAULT_GUILD)
 
 
     @commands.Cog.listener()
@@ -42,3 +47,22 @@ class Digicord(commands.Cog):
         embed = discord.Embed.from_dict(contents)
         await channel.send(embed=embed)
 
+
+    @commands.guild_only()
+    @commands.admin()
+    @commands.command(name="set_spawn_channel")
+    async def set_spawn_channel(self, ctx: commands.Context, channel:discord.TextChannel):
+        """Sets which channel the bot spawns Digimon in.
+        Parameters
+        ----------
+        channel: discord.TextChannel
+            The channel that digimon will appear in.
+        """
+        await self._conf.guild(ctx.guild).spawn_channel.set(channel.id)
+
+        contents = dict(
+            title="Set Spawn Channel: Success",
+            description=f"Spawn channel set to {channel.name}"
+            )
+        embed = discord.Embed.from_dict(contents)
+        await ctx.send(embed=embed)
